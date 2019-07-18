fd3f72d2298fb5a3d979465106bcbdee4de73ffe
Erik Aderstedt <erik@aderstedtsoftware.com>
2019-07-18 11:17:58 +0200

    Initial Commit

---
+++ Dubbelbokningar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dubbelbokningar
+//
+//  Created by Erik Aderstedt on 2019-07-18.
+//  Copyright © 2019 Aderstedt Software AB. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
